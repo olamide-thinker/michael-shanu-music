598be0786e9c9704307a4f4c512b7706ae92ae0f
EducentyOlamide <107129970+EducentyOlamide@users.noreply.github.com>
2024-09-10 09:14:17 +0100

    kept secrets safe

--- src/firebaseConnection/firebase.ts
+++ src/firebaseConnection/firebase.ts
@@ -12,13 +12,13 @@ import { getStorage } from "firebase/storage";
 // };
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCM1iCaTpbaoUK0rQTvbquz2z34mZRQ1t0",
-  authDomain: "michael-shanu-music.firebaseapp.com",
-  projectId: "michael-shanu-music",
-  storageBucket: "michael-shanu-music.appspot.com",
-  messagingSenderId: "569346015289",
-  appId: "1:569346015289:web:602a9ab0aabf972706cde8",
-  measurementId: "G-E4780K2EDM",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
