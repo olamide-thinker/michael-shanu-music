b66aec750b421c501d3a5e41f22102aa5ff59014
EducentyOlamide <107129970+EducentyOlamide@users.noreply.github.com>
2024-09-10 07:58:18 +0100

    fixes

--- src/components/Comps/largeImage.tsx
+++ src/components/Comps/largeImage.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export const LargeImage = ({img}:{img:string}) => {
   return (
     <Image 
-    className='w-auto h-[90vh]'
+    className='sm:w-auto w-full sm:h-[90vh] h-auto'
     src={img} alt={'Image'} width={1080} height={1080}/>
   )
 }
